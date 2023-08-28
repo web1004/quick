3d394b4ff1a1727046497576d76be53c3127b3a2
web1004 <gangsugi@gmail.com>
2023-08-28 11:53:28 +0900

    modify-23.8.28

--- js/scroll2.js
+++ js/scroll2.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	
-  $(window).scroll(function(){ //브라우저에 스크롤이 발생하는 순간부터 
 
-    let pos=$(this).scrollTop();  //스크롤상단 위치값을 저장
+  $(window).scroll(function(){ 
+
+    let pos=$(this).scrollTop();
     $("#QuickBanner").stop().animate({top:pos+350},400);
 
   });
